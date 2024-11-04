ca6e2cb57c1ab1a5217c62d69f1816754181b5b1
Dylpickle2008 <dylanrigby1016@gmail.com>
2024-11-04 08:29:40 -0500

    Create Maze.turtle.py

---
+++ Maze.turtle.py
@@ -0,0 +1,10 @@
+import turtle
+t = turtle.Turtle()
+t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+t.pencolor("black")
+t.fillcolor("indigo")
+t.begin_fill()
+t.forward(100)
+t.left(90)
+t.forward(150)
+t.left(90)
